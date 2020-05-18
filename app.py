a7f88ab69d3e9d81a8f9a400bf1ee3e2f848eed5
Inanc Yigit <inanc@giantfox.nl>
2020-05-18 10:14:12 +0200

    Create app.py
    Add hello world in Python with version check.

---
+++ app.py
@@ -0,0 +1,8 @@
+import sys
+from math import *
+
+print("Running app with Python :", sys.version + " and api_version", sys.api_version)
+print("")
+print("My app:")
+
+print("Hello (virtual) World! =)")
