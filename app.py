3c2ff7964a425446d3375faad15787704263f961
Inanc Yigit <incyi@users.noreply.github.com>
2020-05-18 12:09:01 +0200

    Add test
    Add first unit test

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import sys
-from math import *
 
-print("Running app with Python :", sys.version + " and api_version", sys.api_version)
-print("")
-print("My app:")
+def hello_world():
+    print("Hello (virtual) World! =)")
 
-print("Hello (virtual) World! =)")
+def version():
+    print("Running app with Python :", sys.version + " and api_version", sys.api_version)
+
+def main():
+    hello_world()
+    version()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
