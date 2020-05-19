375cb04c7d8f4e28a60043c173418d81d6564b0e
incyi <incyi@users.noreply.github.com>
2020-05-19 10:10:13 +0200

    Add Travis (#2)
    * Add test
    * Update test_app.py
    * Update .travis.yml

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+from __future__ import print_function
 import sys
-from math import *
 
-print("Running app with Python :", sys.version + " and api_version", sys.api_version)
-print("")
-print("My app:")
 
-print("Hello (virtual) World! =)")
+def hello_world():
+    return 'Hello (virtual) World! =)'
+
+
+def version():
+    version_text = "Running app with Python :", sys.version + " and api_version", sys.api_version
+    return version_text
+
+
+def main():
+    print(hello_world())
+    print(version())
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
